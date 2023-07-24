05802bd4557abc2807628f50494bbecc2ab347df
TzuHan Chen <45888769+TzuHanChen@users.noreply.github.com>
2023-07-24 21:33:43 +0800

    Added favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+import Script from 'next/script';
 import type { AppProps } from 'next/app'
 import { Noto_Sans_TC } from 'next/font/google'
 const noto = Noto_Sans_TC({
@@ -13,11 +15,31 @@ import '@/design-tokens/global.css';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <style jsx global>{`
-        html {
-          font-family: ${noto.style.fontFamily};
-        }
-      `}</style>
+      <Head>
+        {/* favicon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+
+        {/* font */}
+        <style jsx global>{`
+          html {
+            font-family: ${noto.style.fontFamily};
+          }
+        `}</style>
+
+        {/* Google tag (gtag.js) */}
+        {/* <Script async src="https://www.googletagmanager.com/gtag/js?id=G-4FQPG607TH"></Script>
+        <Script>
+          { window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-4FQPG607TH'); }
+        </Script> */}
+      </Head>
+
       <Nav />
       <Component {...pageProps} />
     </>
