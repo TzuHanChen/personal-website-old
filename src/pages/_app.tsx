16fa3550d2fe36191aeeb127ff08e963505eb860
TzuHan Chen <45888769+TzuHanChen@users.noreply.github.com>
2023-07-27 16:20:38 +0800

    Added GA4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,45 +3,64 @@ import Script from 'next/script';
 import type { AppProps } from 'next/app'
 import { Noto_Sans_TC } from 'next/font/google'
 const noto = Noto_Sans_TC({
-  weight: ['400', '700'],
-  style: ['normal'],
-  subsets: ['latin'],
-  display: 'swap',
+	weight: ['400', '700'],
+	style: ['normal'],
+	subsets: ['latin'],
+	display: 'swap',
 })
 
 import Nav from '@/components/nav';
 import '@/design-tokens/global.scss';
 
+function Favicon() {
+	return (
+		<>
+			<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+			<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+			<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+			<link rel="manifest" href="/favicon/site.webmanifest" />
+		</>
+	)
+}
+
+function Font() {
+	return (
+		<>
+			<style jsx global>{`
+				html {
+					font-family: ${noto.style.fontFamily};
+				}
+			`}</style>
+		</>
+	)
+}
+
+function GA4() {
+	return (
+		<>
+			<Script async src="https://www.googletagmanager.com/gtag/js?id=G-4FQPG607TH"></Script>
+			<Script>{`
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+
+				gtag('config', 'G-4FQPG607TH');
+			`}</Script>
+		</>
+	)
+}
+
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        {/* favicon */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
+	return (
+		<>
+			<Head>
+				<Favicon />
+				<Font />
+				<GA4 />
+			</Head>
 
-        {/* font */}
-        <style jsx global>{`
-          html {
-            font-family: ${noto.style.fontFamily};
-          }
-        `}</style>
-
-        {/* Google tag (gtag.js) */}
-        {/* <Script async src="https://www.googletagmanager.com/gtag/js?id=G-4FQPG607TH"></Script>
-        <Script>
-          { window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-4FQPG607TH'); }
-        </Script> */}
-      </Head>
-
-      <Nav />
-      <Component {...pageProps} />
-    </>
-  )
+			<Nav />
+			<Component {...pageProps} />
+		</>
+	)
 }
