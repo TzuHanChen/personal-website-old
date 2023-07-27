c9f4ab43ad5e968731c7084f24d31dab32f648e5
TzuHan Chen <45888769+TzuHanChen@users.noreply.github.com>
2023-07-27 15:35:41 +0800

    Text section layout DONE

--- src/layouts/text-section.tsx
+++ src/layouts/text-section.tsx
@@ -1,3 +1,9 @@
+import styles from './text-section.module.scss';
+
 export default function TextSection({ ...props }) {
-	return ( <></> );
+	return (
+		<section className={styles["text-section"]}>
+			{ props.children }
+		</section>
+	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,11 @@ import SEO from '@/lib/seo'
 
 // import Nav from '@/components/nav'
 import Text, { TextShowcase } from '@/components/text'
-// import { ButtonShowcase } from '@/components/button'
+import Button, { ButtonShowcase } from '@/components/button'
 // import { CardShowcase } from '@/components/card'
 
 import HeroSection from '@/layouts/hero-section'
+import TextSection from '@/layouts/text-section'
 
 export default function Home() {
 	return (
@@ -27,6 +28,14 @@ export default function Home() {
 					</Text>
 					<Text type="h2" align="center">之前是前端網頁實習生，現在的目標是成為前端工程師</Text>
 				</HeroSection>
+
+				<TextSection>
+					<Text type="h2">關於我</Text>
+					<Text>參與過產品開發流程的各項工作後，我選擇以前端工程為本人職業，並期許自己能夠利用過往經驗，與來自各領域的同事順利合作。</Text>
+					<Text align="right">
+						<Button href="/about">詳細自我介紹</Button>
+					</Text>
+				</TextSection>
 			</main>
 		</>
 	)
