3fc68e1b0656a457c98c5461c6822f4c724bdd53
TzuHan Chen <45888769+TzuHanChen@users.noreply.github.com>
2023-07-28 21:09:09 +0800

    Index page cards DONE

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,15 +22,48 @@ function Project() {
 		<GridSection id="project">
 			<Text type="h2">專案</Text>
 			<Grid marginTop>
+				<Card image="/images/personal-website.png"
+					alt="個人網站" title="個人網站"
+					info="展示專案、文章與經歷的網站"
+					tag="Next.js, TypeScript, SCSS, SWR"
+					button="效能報告"
+					href="https://pagespeed.web.dev/analysis?url=https%3A%2F%2Ftzuhanchen.vercel.app%2F"
+					newtab />
 				<Card image="/images/distraction-loves-me.png"
 					alt="數位分心" title="數位分心"
-					info="數位分心教材與研究團隊的網站" button="瀏覽網站"
+					info="數位分心教材與研究團隊的網站"
+					tag="React, Bootstrap, Fetch"
+					button="瀏覽網站"
 					href="https://distractionlovesme.lab.nycu.edu.tw/"
 					newtab />
 				<Card image="/images/teamie.png"
 					alt="Teamie" title="Teamie"
-					info="開發中的專案團隊媒合平台" button="瀏覽網站"
-					href="https://teamie.tw/"
+					info="開發中的專案團隊媒合平台"
+					tag="Next.js, TypeScript, SCSS"
+					button="敬請期待"
+					href="/"
+					newtab />
+				
+				<Card image="/images/personal-website-old.png"
+					alt="個人網站 (舊)" title="個人網站 (舊)"
+					info="展示專案、文章與經歷的網站"
+					tag="JavaScript, SCSS, HTML"
+					button="前往舊站"
+					href="https://tzuhanchen.github.io"
+					newtab />
+				<Card image="/images/max-value.png"
+					alt="珍食力" title="珍食力"
+					info="協助使用者充分運用食材的手機應用程式原型"
+					tag="Miro, Figma"
+					button="作品說明"
+					href="https://tzuhanchen.github.io/max-value.html"
+					newtab />
+				<Card image="/images/medical-forum.png"
+					alt="醫療論壇" title="醫療論壇"
+					info="供使用者發文討論醫療相關議題的網站"
+					tag="Miro, PHP, MySQL"
+					button="作品說明"
+					href="https://tzuhanchen.github.io/medical-forum.html"
 					newtab />
 			</Grid>
 		</GridSection>
@@ -42,14 +75,25 @@ function Practice() {
 		<GridSection id="practice">
 			<Text type="h2">練習</Text>
 			<Grid marginTop>
+				<Card image="/images/nextjs-blog.png"
+					alt="部落格" title="部落格"
+					info="練習各種網頁渲染方式的網站"
+					tag="Next.js, TypeScript, CSS, Markdown"
+					button="作品展示"
+					href="https://nextjs-blog-tzuhanchen.vercel.app"
+					newtab />
 				<Card image="/images/tic-tac-toe.png"
 					alt="井字遊戲" title="井字遊戲"
-					info="可以穿梭時空的井字遊戲" button="作品展示"
+					info="可以穿梭時空的井字遊戲"
+					tag="React"
+					button="作品展示"
 					href="https://tzuhanchen.github.io/tic-tac-toe"
 					newtab />
 				<Card image="/images/api-connection.png"
 					alt="API 串接" title="API 串接"
-					info="利用三種方法隨機讀取三種資料" button="作品展示"
+					info="利用三種方法隨機讀取三種資料"
+					tag="XHR, Fetch, Axios"
+					button="作品展示"
 					href="https://tzuhanchen.github.io/api-connection"
 					newtab />
 			</Grid>
@@ -64,7 +108,7 @@ function Article() {
 			<Grid marginTop>
 				<Card image="/images/arete-internship.png"
 					alt="亞瑞特實習" title="亞瑞特實習"
-					info="在亞瑞特實習的期間撰寫的實習日誌與任務成果"
+					info="實習日誌與任務成果"
 					button="閱讀紀錄"
 					href="https://tzuhanchen.github.io/arete-internship"
 					newtab/>
